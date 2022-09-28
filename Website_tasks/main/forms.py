8bcff3225fbf01ef64befa6b26b9266bdb73d3f7
Anzhalika Turlak <293852647@mail.ru>
2022-09-28 16:26:10 +0300

    task is to transfer the working field

--- Website_tasks/main/forms.py
+++ Website_tasks/main/forms.py
@@ -1,8 +1,12 @@
 from .models import Task
-from django.forms import ModelForm
+from django.forms import ModelForm, widgets, TextInput
 
 
 class TaskForm(ModelForm):
     class Meta:
         model = Task
-        fields =["title", "task", "comment"]
+        fields = ["title", "task", "comment"]
+        widgets = {"title": TextInput(attrs={'class': 'form-control', 'placeholder': 'Введите название'}),
+                   "task": TextInput(attrs={'class': 'form-control', 'placeholder': 'Введите описание задачи'}),
+                   "comment": TextInput(attrs={'class': 'form-control', 'placeholder': 'Введите комментарий'})}
+
